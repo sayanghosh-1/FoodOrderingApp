841be99c924c26ab24c5a42c1ff5ce26bed336c0
Abhijeet Burman <abhiburman20@gmail.com>
2020-12-19 11:56:19 +0530

    test

---
+++ DBLL/DbConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace FoodOrderingApp.DBLL
+{
+    class DbConnection
+    {
+        SqlConnection con = null;
+        SqlCommand cmd = null;
+        public SqlConnection EstablishConnection()
+        {
+            string cs = "Data Source=localhost;Initial Catalog=sample;Integrated Security=True;";
+            con = new SqlConnection();
+            con.ConnectionString = cs;
+            return con;
+        }
+        public bool ReadData()
+        {
+            bool SuccessFlag = false;
+            cmd = new 
+        }
+}
